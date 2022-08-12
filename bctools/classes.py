14b306046ce0a9612d52ff6bfa5708eb898736e2
idk25 <iwo.kucinski@gmail.com>
2022-08-12 13:05:25 +0100

    Formatting

--- bctools/classes.py
+++ bctools/classes.py
@@ -14,35 +14,26 @@ from functools import wraps
 # TODO add simple summary print (no of barcodes, no of barcodes per cell with some deviations?)
 # probably best if this summary is stored but then updated with every filtering step
 
+
 def load_barcodes(file):
     """Loads a .csv files with barcodes and convert automatically to CBUSeries or CBseries"""
 
     data = pd.read_csv(file)
     cols = data.columns.tolist()
-    if cols == ['CBC', 'Barcode', 'UMI', '0']:
-        ser = pd.Series(data['0'])
-        ind =  pd.MultiIndex.from_frame(data.loc[:, ['CBC', 'Barcode', 'UMI']])
+    if cols == ["CBC", "Barcode", "UMI", "0"]:
+        ser = pd.Series(data["0"])
+        ind = pd.MultiIndex.from_frame(data.loc[:, ["CBC", "Barcode", "UMI"]])
         ser.index = ind
         return CBUSeries(ser)
 
-    elif cols == ['CBC', 'Barcode', '0']:
-        ser = pd.Series(data['0'])
-        ind =  pd.MultiIndex.from_frame(data.loc[:, ['CBC', 'Barcode']])
+    elif cols == ["CBC", "Barcode", "0"]:
+        ser = pd.Series(data["0"])
+        ind = pd.MultiIndex.from_frame(data.loc[:, ["CBC", "Barcode"]])
         ser.index = ind
         return CBSeries(ser)
 
     else:
-        raise Exception('This does not look like CBUSeries or CBSeries saved')
-
-
-def check_CB_index(f):
-    """Decorator to check the index before running some CBU-specific methods"""
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if args[0].index.names != ['CBC', 'Barcode']:
-            raise Exception('The multiindex is not "CBC", "Barcode"')
-        return f(*args, **kwargs)
-    return wrapper
+        raise Exception("This does not look like CBUSeries or CBSeries saved")
 
 
 def filter_series(series, groupby=None, labels=["CBC", "Barcode", "UMI"], min_counts=0):
@@ -80,6 +71,7 @@ def filter_series(series, groupby=None, labels=["CBC", "Barcode", "UMI"], min_co
     indexSUB = series.index.droplevel(level=todrop)
     return series[indexSUB.isin(filtered.index)]
 
+
 def plot_groupby_hist(series, groupby, bins=50, vmax=None):
     """Plots histogram of reads, grouped as indicated in the groupby argument"""
 
@@ -89,13 +81,26 @@ def plot_groupby_hist(series, groupby, bins=50, vmax=None):
     if vmax is None:
         vmax = grouped.max()
     ticks = ceil(np.log10(vmax)) + 1
-    vmax = 10**(ticks-1)
+    vmax = 10 ** (ticks - 1)
     logpos = np.logspace(0, np.log10(vmax), ticks)
     plt.xticks(range(ticks), logpos)
     # plt.xscale('log')
     plt.yscale("log")
     plt.title(f"Number of reads per {groupby} combination")
     plt.show()
+
+
+def check_CB_index(f):
+    """Decorator to check the index before running some CBU-specific methods"""
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if args[0].index.names != ["CBC", "Barcode"]:
+            raise Exception('The multiindex is not "CBC", "Barcode"')
+        return f(*args, **kwargs)
+
+    return wrapper
+
 
 class CBSeries(pd.Series):
     def __init__(self, *args, **kwargs):
@@ -112,9 +117,11 @@ class CBSeries(pd.Series):
     @check_CB_index
     def filter_by_UMI(self, groupby="Barcode", min_counts=0):
         labels = ["CBC", "Barcode"]
-        return filter_series(self, groupby=groupby, labels=labels, min_counts=min_counts)
+        return filter_series(
+            self, groupby=groupby, labels=labels, min_counts=min_counts
+        )
 
-    def plot_hist(self, groupby='Barcode', *args, **kwargs):
+    def plot_hist(self, groupby="Barcode", *args, **kwargs):
         plot_groupby_hist(self, groupby=groupby, *args, **kwargs)
 
     @check_CB_index
@@ -129,13 +136,16 @@ class CBSeries(pd.Series):
                 max_counts = max(counts)
                 counts = counts[counts >= (dispr_filter * max_counts)]
             barcodes = counts.index.tolist()
-            row = pd.DataFrame({'Barcode_list' : [barcodes], 'Barcode_n' : len(barcodes)}, index=[i])
+            row = pd.DataFrame(
+                {"Barcode_list": [barcodes], "Barcode_n": len(barcodes)}, index=[i]
+            )
             df = pd.concat([df, (row)])
 
         def catl(x):
-            ''' Function for concatenating strings '''
-            return('-'.join(x))
-        df['Barcode'] = df['Barcode_list'].apply(func = catl)
+            """Function for concatenating strings"""
+            return "-".join(x)
+
+        df["Barcode"] = df["Barcode_list"].apply(func=catl)
         return df
 
     def save_barcodes(self, *args, **kwargs):
@@ -144,11 +154,13 @@ class CBSeries(pd.Series):
 
 def check_CBU_index(f):
     """Decorator to check the index before running some CBU-specific methods"""
+
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if args[0].index.names != ['CBC', 'Barcode', 'UMI']:
+        if args[0].index.names != ["CBC", "Barcode", "UMI"]:
             raise Exception('The multiindex is not "CBC", "Barcode", "UMI"')
         return f(*args, **kwargs)
+
     return wrapper
 
 
@@ -162,31 +174,34 @@ class CBUSeries(pd.Series):
     def _constructor(self):
         return CBUSeries
 
-    def plot_hist(self, groupby='Barcode', *args, **kwargs):
+    def plot_hist(self, groupby="Barcode", *args, **kwargs):
         plot_groupby_hist(self, groupby=groupby, *args, **kwargs)
-
 
     @check_CBU_index
     def filter_by_reads(self, groupby="Barcode", min_counts=0):
         """Filters by minimum expected number of reads. With the groupby column, level combinations are pertmitted"""
         labels = ["CBC", "Barcode", "UMI"]
-        return filter_series(self, groupby=groupby, labels=labels, min_counts=min_counts)
+        return filter_series(
+            self, groupby=groupby, labels=labels, min_counts=min_counts
+        )
 
     @check_CBU_index
     def filter_by_hamming(self, which="Barcode", min_distance=2):
 
         counts = self.groupby([which]).sum()
-        tokeep, toreject, ties = hamming_filter(counts=counts, min_distance=min_distance)
+        tokeep, toreject, ties = hamming_filter(
+            counts=counts, min_distance=min_distance
+        )
         if len(ties) > 0:
             print(f"Ties detected between: {ties}")
 
-        if which == 'CBC':
+        if which == "CBC":
             return self.loc[tokeep, :, :]
 
-        if which == 'Barcode':
+        if which == "Barcode":
             return self.loc[:, tokeep, :]
 
-        if which == 'UMI':
+        if which == "UMI":
             return self.loc[:, :, tokeep]
 
     @check_CBU_index
--- bctools/tests/test_larry.py
+++ bctools/tests/test_larry.py
@@ -6,16 +6,17 @@ import bctools as bc
 HERE = Path(__file__).parent
 # assets_path = HERE / "assets/"
 
+
 def test_load_larry():
-    file1 = str(HERE / 'data/SLX-22398.SITTA1.s_1.r_1_small.fq')
-    file2 = str(HERE / 'data/SLX-22398.SITTA1.s_1.r_2_small.fq')
-    files = {'r1' : file1,
-             'r2' : file2}
+    file1 = str(HERE / "data/SLX-22398.SITTA1.s_1.r_1_small.fq")
+    file2 = str(HERE / "data/SLX-22398.SITTA1.s_1.r_2_small.fq")
+    files = {"r1": file1, "r2": file2}
     bcu = bc.get_barcodes(files)
 
     # a = 1
     # b = 2
     # assert a == b
+
 
 def test_BCU_filter_by_reads():
     pass
