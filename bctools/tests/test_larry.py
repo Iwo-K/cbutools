e89d5ef703f63837e7897b2fb4256a30fa34c356
idk25 <iwo.kucinski@gmail.com>
2022-08-18 16:05:09 +0100

    Larry tests added

--- bctools/tests/test_larry.py
+++ bctools/tests/test_larry.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env ipython
 from pathlib import Path
 import bctools as bc
+import pandas as pd
 
 
 HERE = Path(__file__).parent
 # assets_path = HERE / "assets/"
 
 
-def test_load_larry():
+def test_load():
     file1 = str(HERE / "data/SLX-22398.SITTA1.s_1.r_1_small.fq")
     file2 = str(HERE / "data/SLX-22398.SITTA1.s_1.r_2_small.fq")
     files = {"r1": file1, "r2": file2}
     bcu = bc.get_barcodes(files)
 
-    # a = 1
-    # b = 2
-    # assert a == b
+def test_cutadapt():
+    file1 = str(HERE / "data/mock_larry_cutadapt_r1.fq")
+    file2 = str(HERE / "data/mock_larry_cutadapt_r2.fq")
+    files = {"r1": file1, "r2": file2}
+    cbu = bc.get_barcodes(files).sort_index()
 
+    ind = pd.MultiIndex.from_tuples([
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATGAA'),
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATGAT'),
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATGGT'),
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATGCT'),
+        ('TTTCTGAAGTATTCCG', 'GGGGTGCCGCCATTGGACTCGAGAAGCCGTTGGCAGGGTT', 'CGATCAGATGGT'),
+        ('GGGGTGAAGTATTCCG', 'CCCCTGGGGGCATTGGACTCGAGAAGCCGTTGGCAGAAAA', 'CGATCAGATGGT')
+    ])
+    ref = pd.Series([1,1,1,1,1,1], index=ind)
+    ref = ref.sort_index()
 
-def test_BCU_filter_by_reads():
-    pass
+    assert cbu.equals(ref)
+
+def test_count():
+    file1 = str(HERE / "data/mock_larry_counting_r1.fq")
+    file2 = str(HERE / "data/mock_larry_counting_r2.fq")
+    files = {"r1": file1, "r2": file2}
+    cbu = bc.get_barcodes(files).sort_index()
+
+    ind = pd.MultiIndex.from_tuples([
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATAAA'),
+        ('AACCTGAAGTATTCCG', 'CACGTGCCGCCAAATCACTGCAGAAGCCGTTGGCAGCCAA', 'CGATCAGATGGG'),
+        ('AACCTGAAGTATTCCG', 'GGGGTGCCGCCATTGGACTCGAGAAGCCGTTGGCAGGGTT', 'CGATCAGATCCC'),
+        ('TTGGAGAAGTATTCCG', 'GGGGTGCCGCCATTGGACTCGAGAAGCCGTTGGCAGGGTT', 'CGATCAGATCCC'),
+        ('AAAAAGAAGTATTCCG', 'CCCCTGGGGGCATTGGACTCGAGAAGCCGTTGGCAGAAAA', 'CGATCAGATAAA'),
+        ('AAAAAGAAGTATTCCG', 'CCCCTGGGGGCATTGGACTCGAGAAGCCGTTGGCAGAAAA', 'CGATCAGATTTT')
+        # ('', '', ''),
+    ])
+    ref = pd.Series([2,1,1,4,5,3], index=ind)
+    ref = ref.sort_index()
+
+    assert cbu.equals(ref)
